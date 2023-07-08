a44f7ae027baaedabf9a7682f087c8da493c5bcb
amiyoghosh00 <amiyoghosh00@gmail.com>
2023-07-08 23:50:35 +0530

    Location & Skills

---
+++ app/Filament/Resources/SkillResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SkillResource\Pages;
+use App\Filament\Resources\SkillResource\RelationManagers;
+use App\Models\Skill;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
+
+class SkillResource extends Resource
+{
+    protected static ?string $model = Skill::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                    ->schema([
+                        TextInput::make('name')->reactive()->afterStateUpdated(fn ($state, callable $set) => $set('seo_title', "Remote jobs for {$state}")),
+                        FileUpload::make('seo_image'),
+                        TextInput::make('seo_title'),
+                        Textarea::make('seo_keywords'),
+                        Textarea::make('seo_description'),
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                ImageColumn::make('seo_image'),
+                TextColumn::make('created_at')->since(),
+                TextColumn::make('updated_at')->since(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageSkills::route('/'),
+        ];
+    }
+}
--- database.md
+++ database.md
@@ -16,3 +16,35 @@ Jobs
 - seo_description -> nullable
 - source
 - status -> pending, approved, disabled
+
+Company
+- logo
+- name
+- description
+- others
+- seo_image -> nullable
+- seo_title -> nullable
+- seo_keyword -> nullable
+- seo_description -> nullable
+
+Category
+- name
+- seo_image -> nullable
+- seo_title -> nullable
+- seo_keyword -> nullable
+- seo_description -> nullable
+
+Skills
+- name
+- seo_image -> nullable
+- seo_title -> nullable
+- seo_keyword -> nullable
+- seo_description -> nullable
+
+location
+- name
+- seo_image -> nullable
+- seo_title -> nullable
+- seo_keyword -> nullable
+- seo_description -> nullable
+
